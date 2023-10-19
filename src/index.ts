16e354755a157464b7a741081d21e12c67cf29f6
rashaler <richard.shaler@gmail.com>
2023-10-19 10:01:23 -0700

    Add .state and .decorate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Bun Dev, I am going to build RESTFUL APIs")
+.state({
+  id: 1,
+  email: 'jane@gmail.com'
+})
+.decorate('getDate', () => Date.now())
+.get('/post/:id', ({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
+.post('/post', ({body,set,store}) => {
+  console.log(store)
+  set.status = 201
+  return body
+})
+.get('/track/*', () => {return 'Track Route'})
+.get('/tracks', ({store, getDate}) => {
+  console.log(store)
+  console.log(getDate())
+  return new Response(JSON.stringify({
+    "tracks": [
+      'Amazing Grace',
+      'Without Him',
+      'Where He Leads'
+    ]
+  }), {
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
